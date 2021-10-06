41128b7d038b95655ca18948d6735d12762db1a6
Heidi Fryzell <hfryzell@gmail.com>
2021-10-06 09:28:45 -0400

    centered layout with flexbox

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ const newCardButton = document.querySelector("#new-card-button");
 let messageBlock = document.querySelector("#message-el");
 let cardsBlock = document.querySelector("#cards-el");
 let sumBlock = document.querySelector("#sum-el");
+let newSpan = document.querySelector("#new");
 
 function startGame() {
     renderGame();
 }
 
-function getNewCard(){
+function getNewCard() {
     newCard = Math.floor(Math.random() * (10)) + 2;
     cards.push(newCard);
     sum += newCard;
@@ -24,20 +25,31 @@ function getNewCard(){
     sumBlock.textContent = `Sum: ${sum}`;
 }
 
+function showStartButton() {
+    buttonStart.style.display = "block";
+    newCardButton.style.display = "none";
+    newSpan.style.display = "inline";
+    cards = [];
+    sum = 0;
+}
+
 function renderGame() {
-    if(sum <= 20) {
-        message= "Do you want to draw a new card? 🙂";
-    } else if(sum === 21) {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+        newCardButton.style.display = "block";
+    } else if (sum === 21) {
         message = "Wohoo! You've got Blackjack! 🥳";
         hasBlackJack = true;
+        showStartButton()
     } else {
         message = "You're out of the game! 😭";
         isAlive = false;
+        showStartButton();
     }
     messageBlock.textContent = message;
 }
 
-buttonStart.addEventListener("click", function(){
+buttonStart.addEventListener("click", function () {
     buttonStart.style.display = "none";
     getNewCard();
     getNewCard();
